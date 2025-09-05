dedcbbdcd0912307a5e6e65c129f1a302354a3ef
Sivakumar <nagireddysivakumar952@gmail.com>
2025-09-05 10:39:05 +0530

    axios-instance

--- Client/src/pages/Auth/Login.jsx
+++ Client/src/pages/Auth/Login.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { Input } from "../../components/Inputs/Input";
 import { Link } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apipaths";
 
 export const Login = () => {
     const [email, setEmail] = useState("");
@@ -12,60 +14,79 @@ export const Login = () => {
 
     const navigate = useNavigate();
 
-    const handleLogin = async (e) => { 
+    const handleLogin = async (e) => {
         e.preventDefault();
-        if(!validateEmail(email)){
+        if (!validateEmail(email)) {
             setError("Enter a valid Email address");
             return;
         }
-        if(!password){
+        if (!password) {
             setError("please enter the password");
             return;
         }
         setError("");
-        //api call
+        // Login API Call
+        try {
+            const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+                email,
+                password,
+            });
+            const { token, user } = response.data;
+
+            if (token) {
+                localStorage.setItem("token", token);
+                navigate("/dashboard");
+            }
+        } catch (error) {
+            if (error.response && error.response.data.message) {
+                setError(error.response.data.message);
+            } else {
+                setError("Something went wrong. Please try again.");
+            }
+        }
+
     }
 
     return (
         <AuthLayout>
 
-                {/* Left: Form */}
-                <div className="w-full lg:w-1/2 flex items-center justify-center px-6 py-10">
-                    <div className="max-w-md w-full">
-                        <h3 className="text-2xl font-semibold text-black mb-2">Welcome Back</h3>
-                        <p className="text-sm text-slate-700 mb-6">Please enter your details to login</p>
+            {/* Left: Form */}
+            <div className="w-full lg:w-1/2 flex items-center justify-center px-6 py-10">
+                <div className="max-w-md w-full">
+                    <h3 className="text-2xl font-semibold text-black mb-2">Welcome Back</h3>
+                    <p className="text-sm text-slate-700 mb-6">Please enter your details to login</p>
 
-                        <form onSubmit={handleLogin} className="space-y-4">
-                            <Input
-                                value={email}
-                                onChange={({ target }) => setEmail(target.value)}
-                                label="Email Address"
-                                placeholder="john@example.com"
-                                type="text"
-                            />
-                            <Input
-                                value={password}
-                                onChange={({ target }) => setPassword(target.value)}
-                                label="Password"
-                                placeholder="Min 6 characters"
-                                type="password"
-                            />
-                            {error && <p className="text-red-500 text-sm">{error}</p>}
+                    <form onSubmit={handleLogin} className="space-y-4">
+                        <Input
+                            value={email}
+                            onChange={({ target }) => setEmail(target.value)}
+                            label="Email Address"
+                            placeholder="john@example.com"
+                            type="text"
+                        />
+                        <Input
+                            value={password}
+                            onChange={({ target }) => setPassword(target.value)}
+                            label="Password"
+                            placeholder="Min 6 characters"
+                            type="password"
+                        />
+                        {error && <p className="text-red-500 text-sm">{error}</p>}
 
-                            <button
-                                type="submit"
-                                className="w-full bg-purple-600 text-white py-2 rounded-md hover:bg-purple-700 transition cursor-pointer"
-                            >
-                                Login
-                            </button>
+                        <button
+                            type="submit"
+                            className="w-full bg-purple-600 text-white py-2 rounded-md hover:bg-purple-700 transition cursor-pointer"
+                        >
+                            Login
+                        </button>
 
-                            <p className="text-sm text-center text-slate-800 mt-4">
-                                Don't have an account?{" "}
-                                <Link to='/signup' className="font-medium text-purple-600 underline">Signup</Link>
-                            </p>
-                        </form>
-                    </div>
+                        <p className="text-sm text-center text-slate-800 mt-4">
+                            Don't have an account?{" "}
+                            <Link to='/signup' className="font-medium text-purple-600 underline">Signup</Link>
+                        </p>
+                    </form>
                 </div>
+            </div>
         </AuthLayout>
     );
 };
--- Client/src/utils/apipaths.js
+++ Client/src/utils/apipaths.js
@@ -1,34 +1,31 @@
-export const BASE_URL="https://localhost:8000";
+export const BASE_URL = "http://localhost:8000/";
 
-export const API_PATHS={
-    AUTH:{
-        LOGIN:"/api/auth/login",
-        SIGNUP:"/api/auth/signup",
-        GET_USER_DATA:"/api/auth/getuser",
-    },
-    INCOME:{
-        
-    },
-    DASHBOARD:{
-        GET_DATA:"/api/dashboard"
-    },
-    EXPENSE:{
-        ADD_EXPENSE:"api/expense/add",
-        DELETE_EXPENSE:"api/expense/:id",
-        GET_EXPENSES:"/api/expense/get",
-        
-    }
-}
+export const API_PATHS = {
+  AUTH: {
+    LOGIN: "/api/auth/login",
+    REGISTER: "/api/auth/register",
+    GET_USER_INFO: "/api/auth/getuser",
+  },
 
+  DASHBOARD: {
+    GET_DATA: "/api/dashboard",
+  },
 
+  INCOME: {
+    ADD_INCOME: "/api/income/add",
+    GET_ALL_INCOME: "/api/income/get",
+    DELETE_INCOME: (incomeId) => `/api/income/${incomeId}`,
+    DOWNLOAD_INCOME: "/api/income/downloadExcel",
+  },
 
-// addIncome,
-//     deleteIncome,
-//     getAllIncome,
-//     downloadIncomeExcel
+  EXPENSE: {
+    ADD_EXPENSE: "/api/expense/add",
+    GET_ALL_EXPENSE: "/api/expense/get",
+    DELETE_EXPENSE: (expenseId) => `/api/expense/${expenseId}`,
+    DOWNLOAD_EXPENSE: "/api/expense/downloadExcel",
+  },
 
-
-// addExpense,
-//     deleteExpense,
-//     getAllExpenses,
-//     downloadExpenseExcel
+  IMAGE: {
+    UPLOAD_IMAGE: "/api/auth/upload-image",
+  },
+};
